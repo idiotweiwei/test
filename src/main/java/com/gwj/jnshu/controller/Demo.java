85d36d7d4bd56c83eea974cdea3c086d5e92759a
idiot <614047483@qq.com>
2019-09-04 17:41:49 +0800

    098

--- src/main/java/com/gwj/jnshu/controller/Demo.java
+++ src/main/java/com/gwj/jnshu/controller/Demo.java
@@ -3,7 +3,7 @@ package com.gwj.jnshu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//1235
 @Controller
 public class Demo {
 
